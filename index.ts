110b97005df3af1a894699fcf0c5f8a90a24b22d
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2024-02-10 14:34:37 +1000

    add cron job

--- index.ts
+++ index.ts
@@ -39,6 +39,7 @@ import feedbackRoute from './src/route/teacher.route/teacher.feedback.route/teac
 import './src/cron/sendFeedBackEmails';
 import './src/cron/processMonthlyFees';
 import './src/cron/processTermFees';
+import './src/cron/sendConsolidatedEmailForParents';
 import commentRoute from './src/route/admin.route/admin.comment.route/admin.comment.route';
 import interactionRoute from './src/route/admin.route/admin.interactions.route/admin.interactions.route';
 import adminInstitutionRoute from './src/route/admin.route/admin.institution.route/admin.institution.route';
--- src/cron/sendConsolidatedEmailForParents.ts
+++ src/cron/sendConsolidatedEmailForParents.ts
@@ -12,8 +12,8 @@ cron.schedule('30 16 * * 0', async () => {
         console.error('Error occurred in cron job: consolidateStudentDataForEmail', error);
     }
 });
-// // Schedule to run every Sunday at 4:30 PM
+// Schedule to run every Sunday at 4:30 PM
 // cron.schedule('* * * * *', async () => {
 //     console.log('Running cron job: Send Feedback Emails');
-//     await sendAutomatedEmails();
+//     await consolidateStudentDataForEmail();
 // });
