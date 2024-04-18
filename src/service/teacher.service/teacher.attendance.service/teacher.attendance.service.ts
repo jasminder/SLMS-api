5f0725e14f69451813ad109e54def42c1f5c1da6
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2024-04-18 16:28:44 +1000

    add select teacher for notices

--- src/service/teacher.service/teacher.attendance.service/teacher.attendance.service.ts
+++ src/service/teacher.service/teacher.attendance.service/teacher.attendance.service.ts
@@ -43,7 +43,8 @@ export async function fetchCheckedInStudentsWithAttendance(termSubjectLevelId: s
                                                 }
                                             }
                                         }
-                                    }
+                                    },
+                                    level: true
                                 }
                             }
                         }
