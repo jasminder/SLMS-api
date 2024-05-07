c8a790bfa42ffbbb1fba805b82b78934391469db
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2024-05-08 09:33:20 +1000

    fix fee student by ud

--- src/service/admin.service/admin.student.service/admin.active.student.service/admin.active.student.service.ts
+++ src/service/admin.service/admin.student.service/admin.active.student.service/admin.active.student.service.ts
@@ -1715,42 +1715,19 @@ export async function findActiveStudentById(id: string) {
     return { activeStudent, siblings };
 }
 export async function findStudentFeeDetails(studentId: number, termId: number) {
-    const studentTermFees = await db.studentTermFee.findMany({
+    const studentTermFees = await db.feePayment.findMany({
         where: {
-            studentId: studentId,
-            termSubjectGroup: {
+            studentTermFee: {
+                studentId,
                 termId
             }
         },
+
         include: {
-            termSubjectGroup: {
-                include: {
-                    fee: true,
-                    subjectGroup: true,
-                    subject: true,
-                    enrollment: {
-                        where: {
-                            studentId
-                        },
-                        select: {
-                            dueDate: true,
-                            subjectEnrollment: {
-                                include: {
-                                    termSubject: {
-                                        select: {
-                                            subject: true
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            feePayment: true
+            feeTemplate: true
         }
     });
-
+    console.log(studentTermFees);
     return studentTermFees;
 }
 
@@ -1803,43 +1780,38 @@ export async function findFeePaymentById(id: string) {
 }
 /*update fee - amount paid made by the admin*/
 export async function updateAmountPaid(id: string, newAmountPaid: string, remarks: string) {
-    // const amountPaid = parseInt(newAmountPaid);
-    // const currentFeePayment = await db.feePayment.findUnique({
-    //     where: { id: +id },
-    //     select: { dueAmount: true, amountPaid: true, creditAmount: true }
-    // });
-    // if (!currentFeePayment) {
-    //     throw customError('Fee payment record not found', 'fail', 400, true);
-    // }
-
-    // // Apply existing credit to reduce due amount
-    // let remainingDueAmount = currentFeePayment.dueAmount - currentFeePayment.creditAmount;
-
-    // // Apply payment to remaining due amount
-    // remainingDueAmount -= amountPaid;
-    // // Calculate new credit amount
-    // let newCreditAmount = 0;
-    // if (remainingDueAmount < 0) {
-    //     newCreditAmount = Math.max(amountPaid - currentFeePayment.dueAmount, 0);
-    //     remainingDueAmount = 0;
-    // }
-
-    // // Update the fee payment record
-
-    // const updatedFeePayment = await db.feePayment.update({
-    //     where: { id: +id },
-    //     data: {
-    //         amountPaid: currentFeePayment.amountPaid + amountPaid,
-    //         dueAmount: remainingDueAmount,
-    //         creditAmount: newCreditAmount,
-    //         status: remainingDueAmount > 0 ? 'PENDING' : 'NODUES',
-    //         method: 'DISCOUNT',
-    //         paidDate: new Date(),
-    //         remarks
-    //     }
-    // });
-
-    // return updatedFeePayment;
+    const amountPaid = parseInt(newAmountPaid);
+    const currentFeePayment = await db.feePayment.findUnique({
+        where: { id: +id },
+        select: { dueAmount: true, amountPaid: true, creditAmount: true }
+    });
+    if (!currentFeePayment) {
+        throw customError('Fee payment record not found', 'fail', 400, true);
+    }
+    // Apply existing credit to reduce due amount
+    let remainingDueAmount = currentFeePayment.dueAmount - currentFeePayment.creditAmount;
+    // Apply payment to remaining due amount
+    remainingDueAmount -= amountPaid;
+    // Calculate new credit amount
+    let newCreditAmount = 0;
+    if (remainingDueAmount < 0) {
+        newCreditAmount = Math.max(amountPaid - currentFeePayment.dueAmount, 0);
+        remainingDueAmount = 0;
+    }
+    // Update the fee payment record
+    const updatedFeePayment = await db.feePayment.update({
+        where: { id: +id },
+        data: {
+            amountPaid: (currentFeePayment?.amountPaid??0) + amountPaid,
+            dueAmount: remainingDueAmount,
+            creditAmount: newCreditAmount,
+            status: remainingDueAmount > 0 ? 'PENDING' : 'NODUES',
+            method: 'DISCOUNT',
+            paidDate: new Date(),
+            remarks
+        }
+    });
+    return updatedFeePayment;
 }
 
 export async function findActiveStudentEnrolledSubjects(studentId: string, termId: string) {
@@ -2129,7 +2101,7 @@ export async function enrollActiveStudent(enrollData: ActiveStudentEnrollDataSch
         const newEnrollment = await db.enrollment.create({
             data: {
                 studentId: enrollData.activeStudentId,
-                termSubjectGroupId: enrollmentItem.termSubjectGroupId,
+                termSubjectGroupId: enrollmentItem.termSubjectGroupId
                 // dueDate: dueDate
             },
             select: { id: true }
