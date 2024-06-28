78233f9ce1f67cf70f72e0995fe57024ad5a19ad
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2024-06-28 19:06:13 +1000

    socket fix

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -4,9 +4,18 @@ import { Server as SocketIOServer } from 'socket.io';
 let io: SocketIOServer | null = null;
 
 export function initSocket(server: HttpServer): void {
-    const origin = process.env.NODE_ENV === 'development'
-        ? ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8080', 'https://slms-client-2aam.vercel.app']
-        : ['https://SLMS.com', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://slms-client-2aam.vercel.app', 'https://akaalshaouni.org','https://stg.akaalshaouni.org'];
+    const origin =
+        process.env.NODE_ENV === 'development'
+            ? ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8080', 'https://slms-client-2aam.vercel.app']
+            : [
+                  'https://SLMS.com',
+                  'http://localhost:5173',
+                  'http://localhost:5174',
+                  'http://localhost:5175',
+                  'https://slms-client-2aam.vercel.app',
+                  'https://akaalshaouni.org',
+                  'https://stg.akaalshaouni.org'
+              ];
 
     io = new SocketIOServer(server, {
         cors: {
@@ -20,6 +29,13 @@ export function initSocket(server: HttpServer): void {
         console.log('A user connected');
         socket.on('disconnect', () => {
             console.log('User disconnected');
+        });
+        socket.on('connect_error', (err) => {
+            console.error('Connection Error:', err);
+        });
+
+        socket.on('error', (err) => {
+            console.error('Socket Error:', err);
         });
     });
 
