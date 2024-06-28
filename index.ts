f7938cdf4a4e6705a3e657c13f69705542c6478c
sethumadhavan kochukrishnan <sethumadh@gmail.com>
2024-06-28 10:37:18 +1000

    cors fix

--- index.ts
+++ index.ts
@@ -84,7 +84,7 @@ app.use(cookieParser());
 const origin =
     process.env.NODE_ENV === 'development'
         ? ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:8080', 'https://slms-client-2aam.vercel.app']
-        : ['https://SLMS.com', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://slms-client-2aam.vercel.app', 'https://akaalshaouni.org'];
+        : ['https://SLMS.com', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://slms-client-2aam.vercel.app', 'https://akaalshaouni.org','https://stg.akaalshaouni.org'];
 
 app.use(
     cors({
